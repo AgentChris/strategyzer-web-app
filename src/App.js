c4158821d327c3056f731465d11634e083fdd31a
chris.lego <poputeacristi@gmail.com>
2018-10-13 17:47:22 +0300

    add initial structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <section className="header">
+          <h1 className="title">The Business Model Canvas</h1>
+          <div className="header-container">
+            <span className="header-input design-input">
+            <label className="label">Designed for:</label>
+            <input type="text" />
+          </span>
+            <span className="header-input design-input">
+            <label className="label">Designed by:</label>
+            <input type="text" />
+          </span>
+            <span className="header-input date">
+            <label className="label">Date:</label>
+            <input type="text" />
+          </span>
+            <span className="header-input version">
+            <label className="label">Version:</label>
+            <input type="text" />
+          </span>
+          </div>
+        </section>
+        <section className="container">
+          <div className="canvas-item key-partners">
+            <span>Key Partners</span>
+            <div contentEditable className="content-info"></div>
+          </div>
+          <section className="canvas-item key-activities">
+            <span>Key Activities</span>
+            <div contentEditable className="content-info"></div>
+          </section>
+          <section className="canvas-item key-resources">
+            <span>Key Resources</span>
+            <div contentEditable className="content-info"></div>
+          </section>
+          <div className="canvas-item customer-relationship">
+            <span>Customer Relationship</span>
+            <div contentEditable className="content-info"></div>
+          </div>
+          <div className="canvas-item customer-segments">
+            <span>Customer Segments</span>
+            <div contentEditable className="content-info"></div>
+          </div>
+          <div className="canvas-item channels">
+            <span>Channels</span>
+            <div contentEditable className="content-info"></div>
+          </div>
+          <div className="canvas-item value-proposition">
+            <span>Value Proposition</span>
+            <div contentEditable className="content-info"></div>
+          </div>
+          <div className="canvas-item cost-structure">
+            <span>Cost Structure</span>
+            <div contentEditable className="content-info"></div>
+          </div>
+          <div className="canvas-item revenue-streams">
+            <span>Revenue Streams</span>
+            <div contentEditable className="content-info"></div>
+          </div>
+        </section>
       </div>
     );
   }
