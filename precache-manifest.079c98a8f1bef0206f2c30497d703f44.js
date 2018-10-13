785e5a7d411ec9fd2012a79737e4df3282eacdb7
chris.lego <poputeacristi@gmail.com>
2018-10-13 13:56:15 +0300

    Updates

---
+++ precache-manifest.079c98a8f1bef0206f2c30497d703f44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f625f8e55d4162d420e4",
+    "url": "/static/js/main.f625f8e5.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "f625f8e55d4162d420e4",
+    "url": "/static/css/main.7bcd7c67.chunk.css"
+  },
+  {
+    "revision": "9c78f7f6c0312c6fc59a84a2ddb277d1",
+    "url": "/index.html"
+  }
+];
